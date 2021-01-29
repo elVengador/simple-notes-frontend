8bdbfe46d91c6d1bec5de2374f9b91776b86de14
elVengador <jimynicanor1001@gmail.com>
2021-01-29 18:47:08 -0500

    change config on port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const app = express();
 
-const PORT = 4000 || process.env.PORT;
+const PORT = process.env.PORT || 4000;
 
 app.use(express.static(path.join(__dirname, 'build')));
 
