e9c9b44f50924b5daee5d8155ef27887f8b7a1b5
elVengador <dev.jimynicanor@gmail.com>
2024-02-11 12:37:14 -0500

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import { defineConfig } from "vite";
 export default defineConfig(() => {
   return {
     define: {
-      __APP_ENV__: process.env.VITE_URI,
+      //   __APP_ENV__: process.env.VITE_URI,
+      "process.env.VITE_URI": JSON.stringify(process.env.VITE_URI),
     },
   };
 });
